d573ee065ae7611a8ee763ab291330ee0d1e81b1
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-09-10 00:49:44 +0500

    fix: stop using tuples. Move predicate to the classes static "is" method

--- src/errorClass.test.ts
+++ src/errorClass.test.ts
@@ -3,14 +3,14 @@ import { it, expect, describe } from 'vitest';
 import { errorClass } from './errorClass.js';
 
 it('should create a class with specified name property', () => {
-  const [UnknownError] = errorClass('UnknownError');
+  const UnknownError = errorClass('UnknownError');
   expect(UnknownError.name).toBe('UnknownError');
 });
 
 describe('instance', () => {
   describe('no arguments', () => {
     it('should have proper name and message properties', () => {
-      const [UnknownError] = errorClass('UnknownError');
+      const UnknownError = errorClass('UnknownError');
       const error = new UnknownError();
       expect(error.name).toBe('UnknownError');
       expect(error.message).toBe('');
@@ -18,7 +18,7 @@ describe('instance', () => {
   });
 
   it('should have proper properties', () => {
-    const [UnknownError] = errorClass<
+    const UnknownError = errorClass<
       [errorText: string, retriesCount: number, cause?: unknown]
     >('UnknownError', (errorText, retriesCount, cause) => {
       return [
@@ -30,23 +30,28 @@ describe('instance', () => {
     expect(error.message).toBe('Unknown error occurred. Retries count: 3. Error text: Ooopsie!');
     expect(error.cause).toStrictEqual(new Error('Just because'));
 
-    const [TimeoutError] = errorClass('TimeoutError', 'Timed out');
+    const TimeoutError = errorClass('TimeoutError', 'Timed out');
 
     const timeoutError = new TimeoutError();
     expect(timeoutError.message).toBe('Timed out');
   });
 
   it('should be instance of Error and its class', () => {
-    const [UnknownError] = errorClass('UnknownError');
+    const UnknownError = errorClass('UnknownError');
     expect(new UnknownError()).toBeInstanceOf(Error);
     expect(new UnknownError()).toBeInstanceOf(UnknownError);
   });
 });
 
-describe('predicate', () => {
+describe('is', () => {
   it('should return true if value is instance of created class', () => {
-    const [UnknownError, is] = errorClass('UnknownError');
-    expect(is(new UnknownError)).toBe(true);
-    expect(is(123)).toBe(false);
+    const UnknownError = errorClass('UnknownError');
+
+    class EnhancedUnknownError extends UnknownError {}
+
+    expect(UnknownError.is(new UnknownError)).toBe(true);
+    expect(UnknownError.is(new EnhancedUnknownError())).toBe(true);
+    expect(UnknownError.is(123)).toBe(false);
+    expect(UnknownError.is(new Error())).toBe(false);
   });
 });
