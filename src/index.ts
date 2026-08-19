36296e4554ceeffc36b1e343c79dfcbfe07cb8ba
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-08-19 17:34:37 +0500

    feat: export ErrorClassOptions and ErrorClassWithDataOptions

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ export {
   type ErrorClass,
   type ToSuperFn,
   type ToSuperType,
+  type ErrorClassOptions,
 } from './errorClass.js';
 export {
   errorClassWithData,
   type ErrorClassWithData,
   type ErrorWithData,
   type ToDataFn,
+  type ErrorClassWithDataOptions,
 } from './errorClassWithData.js';
